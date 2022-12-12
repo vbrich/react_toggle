3b34487c19c0f5d9e9bf8a090817fc785e755763
vbrich <vb_rich@yahoo.com>
2022-12-11 19:07:55 -0800

    more theme playing

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,12 @@
-import profile from "./assets/logo.gif";
 import { useState } from 'react';
 import './App.css';
-import {Card,CardHeader,Switch,Box,Container,Typography,FormGroup,FormControlLabel,CssBaseline} from '@mui/material';
+import {Card,Box,Container,CssBaseline} from '@mui/material';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
-import { styled } from '@mui/material/styles';
-
 import IconButton from '@mui/material/IconButton';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import NightlightOutlinedIcon from '@mui/icons-material/NightlightOutlined';
 
-// MUI Switch
-const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-  width: 62,
-  height: 34,
-  padding: 7,
-  '& .MuiSwitch-switchBase': {
-    margin: 1,
-    padding: 0,
-    transform: 'translateX(6px)',
-    '&.Mui-checked': {
-      color: '#fff',
-      transform: 'translateX(22px)',
-      '& .MuiSwitch-thumb:before': {
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          '#fff',
-        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-      },
-      '& + .MuiSwitch-track': {
-        opacity: 1,
-        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-      },
-    },
-  },
-  '& .MuiSwitch-thumb': {
-    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
-    width: 32,
-    height: 32,
-    '&:before': {
-      content: "''",
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      left: 0,
-      top: 0,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-        '#fff',
-      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-    },
-  },
-  '& .MuiSwitch-track': {
-    opacity: 1,
-    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-    borderRadius: 20 / 2,
-  },
-}));
-
 // Define theme settings
-
 const light = {
   palette: {
     mode: "light",
@@ -112,7 +60,7 @@ const dark = {
       main: '#444444',
     },
     background: {
-      default: '#fff',
+      default: '#1E1E1E',
     },
   },
   overrides: {
@@ -133,8 +81,7 @@ const dark = {
 
 function App() {
 
-  // Light theme is default
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
   
   // This function triggered when switch is clicked
   const changeTheme = () => {
@@ -144,43 +91,33 @@ function App() {
   return (
     <div className="App">
       <ThemeProvider theme={isDarkTheme ? createTheme(dark): createTheme(light)}>
+
         <CssBaseline/>
+        
         <Container>
           <div className="App">
-            <Box component="div" p={1}></Box>
-            <Card>
-              <CardHeader action={<FormGroup>
-                <FormControlLabel
-                  control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked onChange={(changeTheme)} />}
-                  label=""
-                />
-              </FormGroup>}/>
-            </Card>
 
-            <Box component="div" p={1}></Box>
-            <Card>
+          <Box component="div" p={1}></Box>
 
-              <IconButton style={{ color: "black" }} aria-label="light" size="small">
-                <LightModeOutlinedIcon/>
+          <Card style={{ border: "none", boxShadow: "none" }}>
+            <div className="Right">
+              <IconButton 
+                color="inherit" 
+                aria-label="light" 
+                size="small" 
+                onClick={() => {
+                  changeTheme();
+                }}>
+                {isDarkTheme ? <LightModeOutlinedIcon fontSize="inherit"/> : <NightlightOutlinedIcon fontSize="inherit"/>}  
               </IconButton>
-              <IconButton color="primary" aria-label="light" size="small">
-                <NightlightOutlinedIcon/>
-              </IconButton>
-              <IconButton aria-label="light" size="small">
-                <NightlightOutlinedIcon/>
-              </IconButton>
+            </div>
+          </Card>
 
-              <Typography variant = "h3" component = "h3" >
-                <div className="container">
-                  Ribo
-                  <br />
-                </div>
-              </Typography>
-              <img src={profile} alt="profile" className="App-logo" />
-              <Typography className="body1" variant="body1">
-                Dark mode is {isDarkTheme ? "On" : "Off"}
-              </Typography>
-            </Card>
+          <Box component="div" p={1}></Box>
+
+          <Card style={{ border: "none", boxShadow: "none", height: '500px' }}>
+            <p>Hello</p>
+          </Card>
 
           </div>
         </Container>
